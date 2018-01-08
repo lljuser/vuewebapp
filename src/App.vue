9f67d16ed585c33e6c119a960cba978f52fd141a
Bing Bai <bbai@HEYI-MAC03s-iMac.local>
2018-01-08 10:34:54 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-link to="/product">
+      <a href="javascript:;">产品</a>
+    </router-link>
+
+    <router-link to="/market">
+      <a href="javascript:;">市场</a>
+    </router-link>
+
+    <router-link to="/trade">
+      <a href="javascript:;">交易</a>
+    </router-link>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form">
+    <p>ID: {{ id }}</p>
+    <input value="zhangsan"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: ['id'],
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
