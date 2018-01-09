0c257d9544c331f73c5dede653ba3ed187d25feb
Bing Bai <bbai@HEYI-MAC03s-iMac.local>
2018-01-09 10:20:49 +0800

    加入下拉刷新

--- src/components/abs/Product.vue
+++ src/components/abs/Product.vue
@@ -2,18 +2,17 @@
   <div class="hello">
     <h1>产品页</h1>
     <a href="javascript:;"></a>
-    <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore">
-
+    <mt-loadmore :top-method="loadTop" ref="loadmore" :topDistance="70">
+      <div 
+        v-for="(item, index) in list" 
+        :key="index" 
+        v-infinite-scroll="loadMore"
+        infinite-scroll-disabled="loading"
+        infinite-scroll-immediate-check="true"
+        infinite-scroll-distance="55">
+        <ProductItem :item="item" :id="index" />
+      </div>
     </mt-loadmore>
-    <div 
-      v-for="(item, index) in list" 
-      :key="index" 
-      v-infinite-scroll="loadMore"
-      infinite-scroll-disabled="loading"
-      infinite-scroll-immediate-check="true"
-      infinite-scroll-distance="55">
-      <ProductItem :item="item" :id="index" />
-    </div>
   </div>
 </template>
 
@@ -24,41 +23,49 @@ export default {
   name: 'product',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
       list: [],
-      page: 0,
+      page: 1,
       loading: false,
     };
   },
   created() {
-    this.fetchProducts();
+    this.fetchProducts(1, (data) => {
+      this.list = data;
+      this.page = this.page + 1;
+    });
   },
   methods: {
     loadTop() {
-      this.page = 1;
-      this.fetchProducts();
-      // 加载更多数据
-      this.$refs.loadmore.onTopLoaded();
+      setTimeout(() => {
+        this.fetchProducts(1, (data) => {
+          this.list = data;
+          this.page = 2;
+          this.loading = false;
+          this.$refs.loadmore.onTopLoaded();
+        });
+      }, 1000);
     },
     loadMore() {
       this.loading = true;
       setTimeout(() => {
-        this.fetchProducts();
+        this.fetchProducts(this.page, (data) => {
+          if (this.page > 1) {
+            this.list = this.list.concat(data);
+          } else {
+            this.list = data;
+          }
+          this.page = this.page + 1;
+          this.loading = false;
+        });
       }, 1000);
     },
-    fetchProducts() {
-      fetch(`http://10.1.1.35/Demo/DemoProduct/getlist/${this.page}`)
+    fetchProducts(page, callback) {
+      fetch(`http://10.1.1.35/Demo/DemoProduct/getlist/${page}`)
       .then(response => response.json())
       .then((json) => {
         const data = json.data;
         if (data && data.length > 0) {
-          if (this.list.length > 0) {
-            this.list = this.list.concat(json.data);
-          } else {
-            this.list = json.data;
-          }
-          this.page = this.page + 1;
-          this.loading = false;
+          callback(data);
         }
       });
     },
