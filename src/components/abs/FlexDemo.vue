466800e5754a0a3036f31b08ff9271bbeb1ab72e
kzhang <pto.kratos@hotmail.com>
2018-01-09 14:25:00 +0800

    mvdir

--- src/components/abs/FlexDemo.vue
+++ src/components/abs/FlexDemo.vue
@@ -128,15 +128,15 @@
         </div>
 		<h2>小图标</h2>
 		<div class="appH5_panel">
-            <img class="appH5_arrowDown" src="../../assets/arrowDown.png" />
-            <img class="appH5_commentIcon" src="../../assets/commentIcon.png" />
-            <img class="appH5_unfollowIcon" src="../../assets/unfollowIcon.png" />
-            <img class="appH5_followIcon" src="../../assets/followIcon.png" />
+            <img class="appH5_arrowDown" src="../../public/image/arrowdown.png" />
+            <img class="appH5_commentIcon" src="../../public/image/commenticon.png" />
+            <img class="appH5_unfollowIcon" src="../../public/image/unfollowicon.png" />
+            <img class="appH5_followIcon" src="../../public/image/followicon.png" />
 		</div>
 		<h2>顶部有返回按钮的页面图标</h2>
 		<div class="appH5_panel">
-            <img class="appH5_returnIcon" src="../../assets/returnIcon.png" />
-            <img class="appH5_threeDot" src="../../assets/threeDot.png" />
+            <img class="appH5_returnIcon" src="../../public/image/returnicon.png" />
+            <img class="appH5_threeDot" src="../../public/image/threedot.png" />
 		</div>
 		<h2>分割线</h2>
 		<div class="appH5_panel">
