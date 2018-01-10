ca5be80d017a86446ac2ef7253f4a417b5801db9
ljliu <lijun.liu@sail-fs.com>
2018-01-10 11:56:41 +0800

    update

--- src/components/abs/Market.vue
+++ src/components/abs/Market.vue
@@ -62,13 +62,7 @@ import Vue from 'vue';
 import VueHighcharts from 'vue-highcharts';
 import Highcharts from 'highcharts';
 
-// load these modules as your need
-import loadStock from 'highcharts/modules/stock';
-import loadMap from 'highcharts/modules/map';
-import loadDrilldown from 'highcharts/modules/drilldown';
-// some charts like solid gauge require `highcharts-more.js`, you can find it in official demo.
-import loadHighchartsMore from 'highcharts/highcharts-more';
-import loadSolidGauge from 'highcharts/modules/solid-gauge';
+// load these modules as your need 
 import * as chartTheme from '@/public/js/chartTheme';
 import * as webApi from '@/config/api'; 
  
