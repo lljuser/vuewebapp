b64024fd5e969779e826f428505d0cd50d07b948
Ivan Yang <xiaofeng.yang@sail-fs.com>
2018-01-12 19:01:07 +0800

    fix issue

--- src/components/expert/WorkHistory.vue
+++ src/components/expert/WorkHistory.vue
@@ -15,14 +15,14 @@
                 <div class="ep_title_div">
                     <span>部门</span>
                 </div>
-                <input class="ep_align_right ep_input fr ep_font32" type="text" placeholder="请输入" v-model="workHistory.Department" />
+                <input class="ep_align_right ep_input fr ep_font32" type="text" placeholder="请输入" v-model.trim="workHistory.Department" />
             </div>
             <div class="ep_part_item ep_part_item_border ep_overhide">
                  <div class="ep_title_div">
                     <span>职位</span>
                     <span class="ep_marginTop5 ep_marginLeft10">*</span>
                 </div>
-                <input class="ep_align_right ep_input fr ep_font32" type="text" placeholder="请输入" v-model="workHistory.Position" />
+                <input class="ep_align_right ep_input fr ep_font32" type="text" placeholder="请输入" v-model.trim="workHistory.Position" />
             </div>
             <div class="ep_part_item ep_part_item_border ep_overhide">
                  <div class="ep_title_div">
@@ -49,7 +49,7 @@
                 </div>
                 </div>
                 <div>
-                    <textarea class="ep_font32 ep_textarea" type="text" placeholder="请输入工作经历" v-model="workHistory.Description"> </textarea>
+                    <textarea class="ep_font32 ep_textarea" type="text" placeholder="请输入工作经历" v-model.trim="workHistory.Description"> </textarea>
                 </div>
             </div>
             <div class="ep_errorTips ep_color_red ep_font24 ep_overhide" v-cloak v-show="isShowError" v-bind:class="[isShowError?'ep_paddingTop60':'']">
@@ -66,26 +66,26 @@
                 <span v-if="isValidElement(id)" class="ep_removeBtn fl" v-on:click="removePopupVisible=true">删除本条信息</span>
             </div>
         </div>
-        <mt-popup v-model="isShowStartTime" position='bottom' modal=true class="ep_popup">
+        <mt-popup v-model.trim="isShowStartTime" position='bottom' modal=true class="ep_popup">
             <div class="ep_overhide ep_padding30 ep_popup_div_btn">
                 <span class="fl ep_color_yellow ep_down_popupBtn" v-on:click="saveYearMonth">确定</span>
                 <span class="fl ep_color_white ep_down_popupBtn" v-on:click="hideDatePicker">取消</span>
             </div>
             <mt-picker v-bind:slots="availableStartTime" v-on:change="onStartTimeChange"></mt-picker>
         </mt-popup>
-        <mt-popup v-model="isShowEndTime" position='bottom' modal=true class="ep_popup">
+        <mt-popup v-model.trim="isShowEndTime" position='bottom' modal=true class="ep_popup">
             <div class="ep_overhide ep_padding30 ep_popup_div_btn">
                 <span class="fl ep_color_yellow ep_down_popupBtn" v-on:click="saveYearMonth">确定</span>
                 <span class="fl ep_color_white ep_down_popupBtn" v-on:click="hideDatePicker">取消</span>
             </div>
             <mt-picker v-bind:slots="availableEndTime" v-on:change="onEndTimeChange"></mt-picker>
         </mt-popup>
-          <mt-popup v-model="removePopupVisible" position='bottom' modal=true class="ep_popup ep_delete_popup ep_align_center ep_font32">
+          <mt-popup v-model.trim="removePopupVisible" position='bottom' modal=true class="ep_popup ep_delete_popup ep_align_center ep_font32">
             <div class="ep_color_grey ep_padding30">确定删除本条信息？</div>
             <div class="ep_padding30 ep_color_orange ep_marginTop2" v-on:click='removeContent'>确定</div>
             <div class="ep_padding30 ep_marginTop2 ep_marginBottom2" v-on:click="removePopupVisible=false">取消</div>
         </mt-popup>
-        <mt-popup v-model="submitPopupVisible" class="ep_submitPopup">
+        <mt-popup v-model.trim="submitPopupVisible" class="ep_submitPopup">
             <div class="ep_divSpinner"><mt-spinner type="snake"></mt-spinner></div>
             <div class="ep_align_center ep_font30 ep_submitColor">提交中...</div>
         </mt-popup>
