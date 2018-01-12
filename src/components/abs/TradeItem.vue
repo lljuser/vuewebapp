1d6ae0efe14a677fb808ad3a7a690b7d36f78fcd
chwu <cunhui.wu@sail-fs.com>
2018-01-12 13:36:00 +0800

    交易列表新的update

--- src/components/abs/TradeItem.vue
+++ src/components/abs/TradeItem.vue
@@ -5,10 +5,20 @@
         <div class='td_elips1'>{{item.SecurityName}}</div>
       </router-link>
     </td>
-    <td class="text-right appH5_color_red">{{item.TotalOffering}}</td>
-    <td class="text-right">{{item.Rating}}</td>
-    <td class="text-right">{{item.WAL}}</td>
-    <td class="text-right td_tworows" v-if="item.Coupon!=null && item.Coupon.split('~').length>1">{{item.Coupon.split('~')[0]}}<br><span>~</span>{{item.Coupon.split('~')[1]}}</td>
+    <td class="text-right td_tworows">
+      <div class="appH5_color_red">{{item.TotalOffering}}</div>
+      <div v-if="item.TradeType=='转让'" class="tradeStatusDiv tradeStatusBlue">转让</div>
+      <div v-if="item.TradeType=='发行'" class="tradeStatusDiv tradeStatusRed">发行</div>
+      <div v-if="item.TradeType=='求购'" class="tradeStatusDiv tradeStatusGreen">求购</div>
+    </td>
+    <td class="text-right td_tworows">
+      <div>{{item.WAL}}</div>
+      <div class="appH5_color_details appH5_font_smaller">{{item.Rating}}</div>
+    </td>
+    <td class="text-right td_tworows" v-if="item.Coupon!=null && item.Coupon.split('~').length>1">
+      <div>{{item.Coupon.split('~')[0]}}</div>
+      <div>~{{item.Coupon.split('~')[1]}}</div>
+    </td>
     <td class="text-right" v-else>{{item.Coupon}}</td>
 
   </tr>
@@ -38,6 +48,29 @@ export default {
     overflow: hidden;
  }
  .td_tworows{
-   padding:0 0.32rem 0 0;
+   padding:0 0.146667rem 0 0;
+ }
+ .tradeStatusDiv{
+   text-align: center;
+   font-size: 10px; 
+   font-weight: lighter;  
+   border-radius: 3px;
+   padding: 0 5px;
+   float: right;
+ }
+ .tradeStatusBlue{
+   background-color: rgba(38, 161, 255, 0.24);
+   border: 1px solid #26A1ff;
+   color: #26A1ff;   
+ }
+  .tradeStatusRed{
+   background-color: rgba(255, 114, 114, 0.24);
+   border: 1px solid #ff7272;
+   color: #ff7272;   
+ }
+  .tradeStatusGreen{
+   background-color: rgba(44, 229, 171, 0.24);
+   border: 1px solid #2ce5ab;
+   color: #2ce5ab;   
  }
 </style>
