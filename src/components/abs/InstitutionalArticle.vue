6922dfe0e08dc3162070a80632bf5b2653e95b9c
qing zou <qing.zou@sail-fs.com>
2018-02-05 15:12:11 +0800

    new page

---
+++ src/components/abs/InstitutionalArticle.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="appH5_body">
+  <div class="product-spinner" v-if="isProductLoading">
+      <mt-spinner type="triple-bounce"></mt-spinner>
+    </div>
+  <div v-else>
+    <div class="articleListContent ep_font32">
+        <div class="ep_marginTop24"></div>
+        <section class="ep_content_div">
+            <div v-cloak>
+                <div class="ep_padding30 ep_part_item_border">
+                    <div class=" ep_overhide">
+                        <span class="fl ep_font28 appH5_color_green">《</span>
+                        <span class="ep_font28 ep_ellipsis fl ep_maxWidth577 appH5_color_green">书名</span>
+                        <span class="fl ep_font28 appH5_color_green">》</span>
+                        <div class="ep_overhide fr ep_lineHeight32 ">
+                            <img class="followImg"/>
+                        </div>
+                    </div>
+                    <div class="divArticleDetail">
+                        <ul class="ep_color_grey ep_font24 ep_decription articleDetail">
+                            <li>
+                                <span class='article_title'>作者：</span>
+                                <span class="ep_ellipsis ep_width517">晓蕾</span>
+                            </li>
+                            <li>
+                                <span class='article_title'>报告分类：</span>
+                                <span class="ep_ellipsis ep_width517">报告分类</span>
+                            </li>
+                            <li>
+                                <span class='article_title'>更新时间：</span>
+                                <span class="ep_ellipsis ep_width517">2018-02-12</span>
+                            </li>
+                            <li>
+                                <span class='article_title'>作品网址：</span>
+                                <!-- <a class="fl ep_ellipsis ep_width300 ep_Link ep_color_orange_important"  v-bind:href="item.Link">{{item.Link}}</a> -->
+                                <span class="fl ep_ellipsis ep_width300 ep_Link ep_color_orange_important">www.baidu.com</span>
+                            </li>
+                        </ul>
+                        <span class="ep_sendMailBtn ep_font24">发送到邮箱</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  </div>
+  
+</div>
+</template>
+
+<script> 
+import * as webApi from '@/config/api';
+import BusUtil from './BusUtil';
+import axios from 'axios';
+export default {
+  name: 'institutionalArticle',
+  props: ['id'],
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      detailInfo:[],
+      newContacts:[],
+      isProductLoading:false,
+    };
+  },
+  created() {
+  }, 
+  mounted() {
+  },
+  methods: {
+ 
+  },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/abs/InstitutionalExperts.vue
@@ -0,0 +1,108 @@
+<template>
+<div class="appH5_body">
+  <div class="product-spinner" v-if="isProductLoading">
+      <mt-spinner type="triple-bounce"></mt-spinner>
+    </div>
+  <div v-else>
+    <div class="appH5_content">
+      <div class="appH5_panel">
+        <div class="relevant-itemA" style="margin-top: 0;">
+            <div style="position: relative;">
+                <img src="../../public/images/userAvatar.png" class="related-image appH5_fl" style="margin-top: 8px;"/>
+            </div>
+            <div class="related-info appH5_fl" style="display: table; margin-left: 5px;">
+                <div style="display: table-cell; vertical-align: middle; height: 80px;">
+                    <div class="relevant-item-name"><span class="appH5_font16 appH5_link">晓蕾</span></div>
+                    <div class="width130 relevant-item-conts">
+                        <div class="relevant-item-info">
+                            <span class="content-truncate width195">部门-职位</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="appH5_fr appH5_followBtn">+关注</div>
+            <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+</template>
+
+<script> 
+import * as webApi from '@/config/api';
+import BusUtil from './BusUtil';
+import util from "@/public/modules/expert/utils";
+import axios from 'axios';
+export default {
+  name: 'institutionalExperts',
+  props: ['id'],
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      detailInfo:[],
+      newContacts:[],
+      isProductLoading:false,
+    };
+  },
+  created() {
+  }, 
+  mounted() {
+  },
+  methods: {
+ 
+  },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.appH5_font12{
+  font-size: 12px;
+}
+.appH5_font16{
+  font-size: 16px;
+}
+.related-image {
+    width: 1.0667rem;
+    height: 1.0667rem;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+    margin-left: 0;
+}
+.briefIntroduction {
+    text-align: justify;
+    padding: 0 20px 20px 20px;
+    line-height: 22px;
+}
+.relevant-item-name {
+    line-height: 26px;
+    font-size: 14px;
+}
+.width130 {
+    width: 130px;
+}
+.width195 {
+    width: 205px;
+}
+.relevant-item-info {
+    line-height: 16px;
+    font-size: 14px;
+}
+.appH5_followBtn,.appH5_unfollowBtn{
+    width: 1.3333rem;
+    height:.61333rem;
+    line-height:.61333rem;
+    border-radius: 2px;
+    -webkit-border-radius: 2px;
+    text-align: center;
+}
+.appH5_followBtn{
+    border: #00FC00 solid 1px;
+    color: #00FC00;
+}
+.appH5_unfollowBtn{
+    border: #666666 solid 1px;
+    color: #666666;
+}
+</style>
