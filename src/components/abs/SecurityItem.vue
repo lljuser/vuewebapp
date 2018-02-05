0676cdba965cea2e486850b2d3728f6d9ab4b9b6
chwu <cunhui.wu@sail-fs.com>
2018-02-05 16:44:51 +0800

    修改框内字的问题

--- src/components/abs/SecurityItem.vue
+++ src/components/abs/SecurityItem.vue
@@ -1,16 +1,16 @@
 <template>
   <tr>
     <td class="text-left">      
-      <div v-if="item.SecurityType=='优'" class="appH5_float_left securityLevelDiv appH5_bg_senior">{{item.SecurityType}}</div>
-      <div v-if="item.SecurityType=='夹'" class="appH5_float_left securityLevelDiv appH5_bg_mezzanine">{{item.SecurityType}}</div>
-      <div v-if="item.SecurityType=='次'" class="appH5_float_left securityLevelDiv appH5_bg_subordin">{{item.SecurityType}}</div>
+      <div v-if="item.SecurityType=='优'" class="appH5_float_left securityLevelDiv appH5_font_smaller appH5_bg_senior">{{item.SecurityType}}</div>
+      <div v-if="item.SecurityType=='夹'" class="appH5_float_left securityLevelDiv appH5_font_smaller appH5_bg_mezzanine">{{item.SecurityType}}</div>
+      <div v-if="item.SecurityType=='次'" class="appH5_float_left securityLevelDiv appH5_font_smaller appH5_bg_subordin">{{item.SecurityType}}</div>
       <router-link :to="`../../securityDetail/${ item.NoteId}`" class="appH5_color_link appH5_float_left" style="width:80%"> 
        <div class='td_elips1'> {{ item.Description }}</div>
       </router-link>
     </td>
     <td class="text-left appH5_color_red" style="font-size:17px">
-      <div v-if="item.RepaymentOfPrincipal=='摊'" class="appH5_float_left securityLevelDiv appH5_bg_amortization">{{item.RepaymentOfPrincipal}}</div>      
-      <div v-if="item.RepaymentOfPrincipal=='过'" class="appH5_float_left securityLevelDiv appH5_bg_passthrough">{{item.RepaymentOfPrincipal}}</div>      
+      <div v-if="item.RepaymentOfPrincipal=='摊'" class="appH5_float_left securityLevelDiv appH5_font_smaller appH5_bg_amortization">{{item.RepaymentOfPrincipal}}</div>      
+      <div v-if="item.RepaymentOfPrincipal=='过'" class="appH5_float_left securityLevelDiv appH5_font_smaller appH5_bg_passthrough">{{item.RepaymentOfPrincipal}}</div>      
       <div>{{item.Principal}}</div>
       </td>
     <td style="color:white" class="text-right"><div>{{ item.CurrentCoupon }}</div></td>
@@ -56,10 +56,10 @@ export default {
 .securityLevelDiv {
   width: 17px;
   height: 17px;
-  font-size: 12px;
   text-align: center;
   margin-top: 4px;
   margin-right: 4px;
   color: #ffffff;
+  line-height: 16px;
 }
 </style>
