6068976894e133c3d5acaa775705dc862fe1c35a
mxdong <mingxia.dong@sail-fs.com>
2018-02-06 17:04:49 +0800

    InstitutionalExperts update

--- src/components/abs/InstitutionalExperts.vue
+++ src/components/abs/InstitutionalExperts.vue
@@ -8,7 +8,10 @@
     <div class="appH5_content" >
       <div class="appH5_panel">
       <mt-loadmore :top-method="loadTop"  ref="loadmore">
-        <div class="relevant-item">
+        <div class="relevant-item"  v-infinite-scroll="loadMore"
+          infinite-scroll-disabled="loading"
+          infinite-scroll-immediate-check="true"
+          infinite-scroll-distance="55">
           <div class="padStyle" v-for="item in expertsInfo">
               <div style="position: relative;">
                 
@@ -35,8 +38,15 @@
               <div class="clearfix"></div>
             </div>
         </div>
+         
       </mt-loadmore>
-
+         <div class="spinner_div" v-if="expertsInfo.length==0">
+          <span  class="nomore">暂无数据</span>
+        </div>
+        <div class="spinner_div" v-else >
+          <van-loading type="spinner" v-if="!noMore" color="white" class="spinner-circle"/>
+          <span v-if="noMore" class="nomore">没有更多了</span>
+        </div>
       </div>
     </div>
 
@@ -44,55 +54,56 @@
 </div>
 </template>
 
-<script> 
-import BusUtil from './BusUtil';
-import Vue from 'vue';
+<script>
+import BusUtil from "./BusUtil";
+import Vue from "vue";
 import util from "@/public/modules/expert/utils";
-import * as webApi from '@/config/api';
-import axios from 'axios';
-import getParams from '../../public/js/getParams';
-import { Toast } from 'mint-ui';
+import * as webApi from "@/config/api";
+import axios from "axios";
+import getParams from "../../public/js/getParams";
+import { Toast } from "mint-ui";
+import "mint-ui/lib/style.css";
+
 export default {
-  name: 'institutionalExperts',
+  name: "institutionalExperts",
   data() {
     return {
-      expertsInfo:[],
-      isExpertsLoading:false,
-      CurrentStatus:[],
+      expertsInfo: [],
+      isExpertsLoading: false,
+      CurrentStatus: [],
       loading: false,
       isFetchExpertsError: false,
       page: 1,
-      pageSize:15,
-      noMore:false,
-      isLoadTop:false,
+      pageSize: 15,
+      noMore: false,
+      isLoadTop: false
     };
   },
   created() {
-        const busUtil = BusUtil.getInstance();
-        busUtil.bus.$emit('showHeader', true);
-        busUtil.bus.$emit('path', '/organDetail/1912');
-        busUtil.bus.$emit('headTitle', '');
-        this.tableFlag=0;
-  }, 
+    const busUtil = BusUtil.getInstance();
+    busUtil.bus.$emit("showHeader", true);
+    busUtil.bus.$emit("path", "/organDetail/1912");
+    busUtil.bus.$emit("headTitle", "");
+    this.tableFlag = 0;
+  },
   mounted() {
     this.isExpertsLoading = true;
     this.timer = setTimeout(() => {
       this.loadFirstPageExperts();
     }, 600);
   },
-  activated(){
+  activated() {
     this.loading = false;
-    this.isExpertsLoading=true;
+    this.isExpertsLoading = true;
     this.expertsInfo = {};
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     const busUtil = BusUtil.getInstance();
-    busUtil.bus.$emit('showHeader', true);
-    busUtil.bus.$emit('path', '/organDetail/1912');
-    busUtil.bus.$emit('headTitle', '');
+    busUtil.bus.$emit("showHeader", true);
+    busUtil.bus.$emit("path", "/organDetail/1912");
+    busUtil.bus.$emit("headTitle", "");
 
-
-    var reLoadData=false;   
-    if(reLoadData){
+    var reLoadData = false;
+    if (reLoadData) {
       this.loadFirstPageExperts();
     }
 
@@ -102,13 +113,13 @@ export default {
 
     this.id = this.$route.params.id;
     if (this.id) {
-        setTimeout(()=>{
-            this.fetchExpertsDetail(this.page,1,data=>{
-                busUtil.bus.$emit('headTitle', '机构专家'); 
-                this.expertsInfo =data;
-                this.isExpertsLoading=false;
-            });
-        },600);
+      setTimeout(() => {
+        this.fetchExpertsDetail(1, 0, data => {
+          busUtil.bus.$emit("headTitle", "机构专家");
+          this.expertsInfo = data;
+          this.isExpertsLoading = false;
+        });
+      }, 600);
     }
   },
   deactivated() {
@@ -120,87 +131,96 @@ export default {
     loadFirstPageExperts(showSpinnerLoad) {
       this.loading = false;
       this.isExpertsLoading = true;
-      if(showSpinnerLoad!=null)this.isExpertsLoading = false;
+      if (showSpinnerLoad != null) this.isExpertsLoading = false;
       setTimeout(() => {
-        this.fetchExpertsDetail(1,0, data => {
-          this.list = data;
+        this.fetchExpertsDetail(1, 0, data => {
+          this.expertsInfo = data;
           this.isExpertsLoading = false;
-          this.page=1;
-          if(data.length<this.pageSize)
-          {
-            this.noMore=true;
+          this.page = 1;
+          if (data.length < this.pageSize) {
+            this.noMore = true;
           }
-          if(showSpinnerLoad!=null) this.$refs.loadmore.onTopLoaded();
+          if (showSpinnerLoad != null) this.$refs.loadmore.onTopLoaded();
         });
       }, 600);
     },
-    fetchExpertsDetail(page,direction,callback) {
-        var url=webApi.Organ.expertList+'/'+ this.$route.params.id;
-        url=url+"/"+direction+"/"+page*this.pageSize+"/"+this.pageSize;
-        axios.post(url).then((response) => {
-            if (response.data.status == "ok") {
-                const data = response.data.data;
-                if(data){
-                     callback(data);
-                     if(data.length==0){ 
-                        this.loading=true;
-                        this.noMore=true;
-                      }
-                      this.isFetchExpertsError = false;
-                      this.isLoadTop=false;
-                } else{
-                    this.doCatch();
-                }
+    fetchExpertsDetail(page, direction, callback) {
+      var url = webApi.Organ.expertList + "/" + this.$route.params.id;
+      url =
+        url +
+        "/" +
+        direction +
+        "/" +
+        page * this.pageSize +
+        "/" +
+        this.pageSize;
+      axios
+        .post(url)
+        .then(response => {
+          if (response.data.status == "ok") {
+            const data = response.data.data;
+            if (data) {
+              callback(data);
+              if (data.length == 0) {
+                this.loading = true;
+                this.noMore = true;
+              }
+              this.isFetchExpertsError = false;
+              this.isLoadTop = false;
+            } else {
+              this.doCatch();
             }
-        }).catch((error) => {
-            this.doCatch();
+          }
+        })
+        .catch(error => {
+          this.doCatch();
         });
-   },
-  loadTop(){
-    this.isLoadTop=true;
-    this.timer = setTimeout(() => {
-      this.loadFirstPageExperts(true);
-    }, 600);   
-  },
-  loadMore(){
-    this.loading = true;
-    this.noMore=false;
-    setTimeout(() => {
-      this.fetchTrades(this.page, 1,data => {
-        this.list = this.list.concat(data);
-        this.page = this.page + 1;
-        this.loading = false;
-      });
-    }, 300);
-  },
-  doCatch(){
-        Toast('服务器繁忙，请重试！');
-        this.isExpertsLoading = false;
-        this.isFetchExpertsError = true;
-        this.loading = false;
-        if(this.isLoadTop){
-          setTimeout(() => {
-            this.$refs.loadmore.onTopLoaded();
-          }, 4000);
-        }
-  },
-  },
+    },
+    loadTop() {
+      this.isLoadTop = true;
+      this.timer = setTimeout(() => {
+        this.loadFirstPageExperts(true);
+      }, 600);
+    },
+    loadMore() {
+      this.loading = true;
+      this.noMore = false;
+      setTimeout(() => {
+        this.fetchExpertsDetail(this.page, 1, data => {
+          this.expertsInfo = this.expertsInfo.concat(data);
+          this.page = this.page + 1;
+          this.loading = false;
+        });
+      }, 600);
+    },
+    doCatch() {
+      Toast("服务器繁忙，请重试！");
+      this.isExpertsLoading = false;
+      this.isFetchExpertsError = true;
+      this.loading = false;
+      if (this.isLoadTop) {
+        setTimeout(() => {
+          this.$refs.loadmore.onTopLoaded();
+        }, 4000);
+      }
+    }
+  }
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.appH5_font12{
+.appH5_font12 {
   font-size: 12px;
 }
-.appH5_font16{
+.appH5_font16 {
   font-size: 16px;
 }
-.appH5_panel{
+.appH5_panel {
   padding: 0;
 }
-.padStyle{
-  padding: 0 .32rem;
-  border-bottom: #3B3A39 solid 1px;
+.padStyle {
+  padding: 0 0.32rem;
+  border-bottom: #3b3a39 solid 1px;
   background-color: black;
 }
 .relevant-item {
@@ -208,59 +228,60 @@ export default {
   width: 100%;
 }
 .related-image {
-    width: 1.0667rem;
-    height: 1.0667rem;
-    border-radius: 50%;
-    -webkit-border-radius: 50%;
-    margin-top: .4rem;
+  width: 1.0667rem;
+  height: 1.0667rem;
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  margin-top: 0.4rem;
 }
-.content-truncate{
+.content-truncate {
   display: block;
   width: 5.7333rem;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
 }
-.appH5_followBtn,.appH5_unfollowBtn{
-    width: 1.3333rem;
-    height:.61333rem;
-    line-height:.61333rem;
-    border-radius: .10667rem;
-    -webkit-border-radius: .10667rem;
-    text-align: center;
-    margin-top: 0.66rem;
-    display: block;
+.appH5_followBtn,
+.appH5_unfollowBtn {
+  width: 1.3333rem;
+  height: 0.61333rem;
+  line-height: 0.61333rem;
+  border-radius: 0.10667rem;
+  -webkit-border-radius: 0.10667rem;
+  text-align: center;
+  margin-top: 0.66rem;
+  display: block;
 }
-.appH5_followBtn{
-    border: #00FC00 solid 1px;
-    color: #00FC00;
+.appH5_followBtn {
+  border: #00fc00 solid 1px;
+  color: #00fc00;
 }
-.appH5_unfollowBtn{
-    border: #666666 solid 1px;
-    color: #666666;
+.appH5_unfollowBtn {
+  border: #666666 solid 1px;
+  color: #666666;
 }
 .related-info {
-  display: table; 
-  margin-left: .26667rem;
+  display: table;
+  margin-left: 0.26667rem;
 }
-.related-info-cont{
-  display: table-cell; 
-  vertical-align: middle; 
+.related-info-cont {
+  display: table-cell;
+  vertical-align: middle;
   height: 1.8667rem;
-  line-height: .55rem;
+  line-height: 0.55rem;
 }
 .authenticated {
-    vertical-align: middle;
-    margin-left: .15rem;
-    background-color: #FF7272;
-    display: inline-block;
-    font-size: 10px;
-    color: #fff !important;
-    padding: .05rem .05rem;
-    border-radius: 2px;
-    -webkit-border-radius: 2px;
-    letter-spacing: 1px;
-    line-height: 14px;
-    margin-top: -3px;
+  vertical-align: middle;
+  margin-left: 0.15rem;
+  background-color: #ff7272;
+  display: inline-block;
+  font-size: 10px;
+  color: #fff !important;
+  padding: 0.05rem 0.05rem;
+  border-radius: 2px;
+  -webkit-border-radius: 2px;
+  letter-spacing: 1px;
+  line-height: 14px;
+  margin-top: -3px;
 }
 </style>
