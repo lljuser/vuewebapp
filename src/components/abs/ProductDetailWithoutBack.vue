eb926575b60b5d867087081de6f917494b249c78
jolonpei <xiaolei.pei@sail-fs.com>
2018-02-07 11:19:32 +0800

    Fix bug of un reinitialize

--- src/components/abs/ProductDetailWithoutBack.vue
+++ src/components/abs/ProductDetailWithoutBack.vue
@@ -179,7 +179,7 @@ export default {
     this.productDetail = {};
     this.publishDate = "";
     this.noteConsTable = "";
-
+    this.showChart=true;
     this.options = {
       title: {
         text: "暂无数据"
@@ -212,6 +212,7 @@ export default {
     }
   },
   updated() {
+    
     var paidList = document.getElementsByClassName("divHasPaid");
     for (var i = 0; i < paidList.length; i++) {
       paidList[i].style.backgroundImage =
