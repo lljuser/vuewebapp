0fb7edf2789712aaa4e2b02df9d766d88de8bbbc
chwu <cunhui.wu@sail-fs.com>
2018-02-28 11:56:52 +0800

    登录页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,188 @@
-<template>
-  <div id="app" class="cnabs-bg">
-    <span style="color:white">登陆页</span>
+<template >
+  <div id="app" class="absLinkBg">
+      <img src="../public/images/loginTitle.png" alt="" style="width:100%">
+    <div style="width:100%">
+    </div>
+    <form @submit.prevent="submit" method="post">
+      <div class="loginPanel">
+          <div class="accountDiv">
+            <div class="accountIcon">
+              <img src="../public/images/username.png" alt="" style="width:100%">
+            </div>
+            <div class="accountInput">
+              <input type="text" name="" id="username" class="inputBack appH5_font_larger" v-model="Username" placeholder="请输入CNABS账号/手机号">
+            </div>
+          </div>
+          <div style="clear:both"></div>
+          <div class="passwordDiv">
+            <div class="passwordIcon">
+              <img src="../public/images/password.png" alt="" style="width:100%">
+            </div>
+            <div class="passwordInput">
+              <input type="password" name="" id="password" class="inputBack appH5_font_larger" v-model="Password" placeholder="请输入密码">
+            </div>
+          </div>
+          <div class="loginBtnDiv">
+            <input type="submit" value="登  录"  class="appH5_btn_larger_blue loginBtnInput"/>
+          </div>
+      </div> 
+    </form>
+    <div style="clear:both"></div>
+    <!-- <div class="moreOpration">
+    <div class="appH5_float_left dynamicLogin">动态密码登录</div>
+    <div class="appH5_float_right registerAccount">注册账户</div>
+    </div> -->
+    <div class="loginBottom">
+      <div class="bottomVersion">
+        <div class="cnabsLogo">
+          <img src="../public/images/appIcon.png" alt="" style="width:100%">
+        </div>
+        <div class="versionInfo">
+            <span class="appH5_font_normal" style="color:#ccc">CNABS移动应用v1.5.0</span>
+        </div>
+      </div>
+    </div>               
   </div>
 </template>
+<style>
+  .inputBack{
+    background-color:#191919;
+    border:none;
+    width:100%;
+    color:white;
+    height: 50px;
+    line-height: 50px;
+    padding-top: 2px;
+  }
+  input::-webkit-input-placeholder{
+      color:white;
+  }
+  input::-moz-placeholder{   /* Mozilla Firefox 19+ */
+      color:white;
+  }
+  input:-moz-placeholder{    /* Mozilla Firefox 4 to 18 */
+      color:white;
+  }
+  input:-ms-input-placeholder{  /* Internet Explorer 10-11 */ 
+      color:white;
+  }
+  input:-webkit-autofill {
+  -webkit-box-shadow: 0 0 0px 1000px #191919 inset;
+  -webkit-text-fill-color: white;
+}
+  .absLinkBg{    
+    position:absolute;
+    height:100%;
+  }
+  .loginPanel{
+    padding-top:40px;
+    width: 100%;
+  }
+  .accountDiv{
+    width:76%;
+    height:54px;
+    border-bottom:solid 1px #444444;
+    margin:0 auto;
+  }
+  .accountIcon{
+    width:20px;
+    float:left;
+    margin:18px 10px;
+  }
+  .accountInput{
+    float:left;
+    width:82%;
+  }
+  .passwordDiv{
+    width:76%;
+    height:54px;
+    border-bottom:solid 1px #444444;
+    margin:0 auto;
+    padding-top:10px;
+  }
+  .passwordIcon{
+    width:20px;
+    float:left;
+    margin:18px 10px;
+  }
+  .passwordInput{
+    float:left;
+    width:82%;
+  }
+  .loginBtnDiv{
+    width:76%;
+    margin:0 auto;
+    padding-top:36px;   
+  }
+  .loginBtnInput{
+    background-color:#F73B09;
+    padding:10px 0;
+    width:100%;
+    height:54px;
+     border-radius: 2px;
+  }
+  .loginBottom{
+    width:100%;
+    height:30px;
+    position:fixed;
+    bottom:0;
+  }
+  .bottomVersion{
+    width:50%;
+    margin:0 auto;
+    height:100%;
+  }
+  .cnabsLogo{
+    float:left;
+    width:8%;
+    padding-top:2px;
+    margin-right:6px;
+  }
+  .versionInfo{
+    float:left;
+    width:84%;
+  }
+  .moreOpration{
+    width: 76%;
+    margin: 0 auto;    
+    color:  #666666;
+    padding-top: 36px;
+  }
+</style>
 
 <script>
- 
-import '@/public/css/theme.css';
+  import 'mint-ui/lib/style.css'
+  import '@/public/css/theme.css';
+  import axios from "axios";
 
-export default {
-  
-};
+  export default { 
+    name:"login",
+    data(){
+      return{
+        Username:"",
+        Password:"",
+      };
+    },
+    methods:{
+      submit:function(){
+        // alert(123)
+        var self=this;
+        var params = new URLSearchParams();
+        params.append('UserName', self.Username);
+        params.append('Password', self.Password);
+        //params.append('returnUrl', self.Password);
+        console.log(self.Username)        
+        console.log(self.Password)
+        // axios({
+        //   url: 'http://localhost:43400/H5Account/Login',
+        //   method: 'post',
+        //   data: params,
+        //   headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded'
+        //   },
+        //   withCredentials: true
+        // })
+      }      
+    },  
+  };
 </script>
